96a12db79b188f2769503c3f919a79f706ddd28f
potatoshreds <a1837777@student.adelaide.edu.au>
2025-03-30 23:22:00 +1030

    bbb

--- Proxy.py
+++ Proxy.py
@@ -55,6 +55,7 @@ while True:
   # Accept connection from client and store in the clientSocket
   try:
     # ~~~~ INSERT CODE ~~~~
+    clientSocket, clientAddress = serverSocket.accept() #Waiting for connection
     # ~~~~ END CODE INSERT ~~~~
     print ('Received a connection')
   except:
@@ -64,6 +65,7 @@ while True:
   # Get HTTP request from client
   # and store it in the variable: message_bytes
   # ~~~~ INSERT CODE ~~~~
+  message_bytes = clientSocket.recv(BUFFER_SIZE) #Receive data connected to
   # ~~~~ END CODE INSERT ~~~~
   message = message_bytes.decode('utf-8')
   print ('Received request:')
@@ -116,6 +118,7 @@ while True:
     # ProxyServer finds a cache hit
     # Send back response to client 
     # ~~~~ INSERT CODE ~~~~
+    
     # ~~~~ END CODE INSERT ~~~~
     cacheFile.close()
     print ('Sent to the client:')
@@ -126,6 +129,8 @@ while True:
     # Create a socket to connect to origin server
     # and store in originServerSocket
     # ~~~~ INSERT CODE ~~~~
+    for line in cacheData: #cycle
+      clientSocket.sendall((line.encode)) #Convert the string to bytes and send it
     # ~~~~ END CODE INSERT ~~~~
 
     print ('Connecting to:\t\t' + hostname + '\n')
