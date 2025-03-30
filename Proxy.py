2df92f50939bea67410d71c52c073a6bc6159781
potatoshreds <a1837777@student.adelaide.edu.au>
2025-03-31 04:09:10 +1030

    666

--- Proxy.py
+++ Proxy.py
@@ -4,6 +4,7 @@ import sys
 import os
 import argparse
 import re
+import time
 
 # 1MB buffer size
 BUFFER_SIZE = 1000000
@@ -20,7 +21,7 @@ proxyPort = int(args.port)
 try:
   # Create a server socket
   # ~~~~ INSERT CODE ~~~~
-  serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.socket-Create a soctet, AF_INT-Use IPv4, socket.SOCK_STREAM-Use TCP
+  serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # socket.socket-Create a soctet, AF_INT-Use IPv4, socket.SOCK_STREAM-Use TCP
   # ~~~~ END CODE INSERT ~~~~
   print ('Created socket')
 except:
@@ -30,7 +31,7 @@ except:
 try:
   # Bind the the server socket to a host and port
   # ~~~~ INSERT CODE ~~~~
-  serverSocket.bind((proxyHost, proxyPort)) #Bind and start connecting socket
+  serverSocket.bind((proxyHost, proxyPort)) # Bind and start connecting socket
   # ~~~~ END CODE INSERT ~~~~
   print ('Port is bound')
 except:
@@ -55,7 +56,7 @@ while True:
   # Accept connection from client and store in the clientSocket
   try:
     # ~~~~ INSERT CODE ~~~~
-    clientSocket, clientAddress = serverSocket.accept() #Waiting for connection
+    clientSocket, clientAddress = serverSocket.accept() # Waiting for connection
     # ~~~~ END CODE INSERT ~~~~
     print ('Received a connection')
   except:
@@ -65,7 +66,7 @@ while True:
   # Get HTTP request from client
   # and store it in the variable: message_bytes
   # ~~~~ INSERT CODE ~~~~
-  message_bytes = clientSocket.recv(BUFFER_SIZE) #Receive data connected to
+  message_bytes = clientSocket.recv(BUFFER_SIZE) # Receive data connected to
   # ~~~~ END CODE INSERT ~~~~
   message = message_bytes.decode('utf-8')
   print ('Received request:')
@@ -106,31 +107,52 @@ while True:
     if cacheLocation.endswith('/'):
         cacheLocation = cacheLocation + 'default'
 
+  # Print and save in full
     print ('Cache location:\t\t' + cacheLocation)
+    
 
     fileExists = os.path.isfile(cacheLocation)
     
     # Check wether the file is currently in the cache
     cacheFile = open(cacheLocation, "r")
     cacheData = cacheFile.readlines()
+    cache_str = ''.join(cacheData)
 
+    # Check expired
+    cache_control_match = re.search(r'Cache-Control:.*?max-age=(\d+)', cache_str, re.IGNORECASE)
+    max_age = int(cache_control_match.group(1)) if cache_control_match else None
+    modified_time = os.path.getmtime(cacheLocation) # Cache effective time
+    age = time.time() - modified_time # This is the age of the cached file
+    print(f"Age of cached files: {int(age)} seconds")
+    if age > max_age:
+      print("File Expiration")
+      raise Exception("This file has expired")
+    
+    
     print ('Cache hit! Loading from cache file: ' + cacheLocation)
     # ProxyServer finds a cache hit
     # Send back response to client 
     # ~~~~ INSERT CODE ~~~~
-    for line in cacheData:#cycle
-      clientSocket.sendall(line.encode()) #Convert the string to bytes and send it
+    for line in cacheData:
+      clientSocket.send(line.encode()) # Convert the string to bytes and send it
     # ~~~~ END CODE INSERT ~~~~
     cacheFile.close()
     print ('Sent to the client:')
-    print ('> ' + cacheData)
+    
+    # Updata print
+    print ('> ' + cache_str)
+    
+    #for line in cacheData:
+    #  print ('> ' + cacheData)
+  
   except:
     # cache miss.  Get resource from origin server
+    print("Maybe we lost cache")
     originServerSocket = None
     # Create a socket to connect to origin server
     # and store in originServerSocket
     # ~~~~ INSERT CODE ~~~~
-    originServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Create a new socket and connect to the specified website
+    originServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a new socket and connect to the specified website
     # ~~~~ END CODE INSERT ~~~~
 
     print ('Connecting to:\t\t' + hostname + '\n')
@@ -139,7 +161,7 @@ while True:
       address = socket.gethostbyname(hostname)
       # Connect to the origin server
       # ~~~~ INSERT CODE ~~~~
-      originServerSocket.connect((address,80)) #Origin socket server is connected to the web IP, 80 is the default value for http
+      originServerSocket.connect((address,80)) # Origin socket server is connected to the web IP, 80 is the default value for http
       # ~~~~ END CODE INSERT ~~~~
       print ('Connected to origin Server')
 
@@ -150,8 +172,8 @@ while True:
       # originServerRequest is the first line in the request and
       # originServerRequestHeader is the second line in the request
       # ~~~~ INSERT CODE ~~~~
-      originServerRequest = f"GET {resource} HTTP/1.1" #Request, sent to the origin server
-      originServerRequestHeader = f"Host: {hostname}\r\n"
+      originServerRequest = f"GET {resource} HTTP/1.1" # Request, sent to the origin server
+      originServerRequestHeader = f"Host: {hostname}\r\n" # Creates an HTTP header
       # ~~~~ END CODE INSERT ~~~~
 
       # Construct the request to send to the origin server
@@ -172,24 +194,40 @@ while True:
 
       # Get the response from the origin server
       # ~~~~ INSERT CODE ~~~~
-      originServerRespone = originServerSocket.recv(BUFFER_SIZE)
+      originServerResponse = originServerSocket.recv(BUFFER_SIZE) # Read once and respond
       # ~~~~ END CODE INSERT ~~~~
 
       # Send the response to the client
       # ~~~~ INSERT CODE ~~~~
-      clientSocket.sendall(originServerRespone)
+      clientSocket.sendall(originServerResponse) # Send HTTP response to the client
       # ~~~~ END CODE INSERT ~~~~
+      
+      # Can it be cached?
+      response_string = originServerResponse.decode('ISO-8859-1')
+      
+      # Cannot cache
+      no_store = re.search(r'Cache-Control:\s*no-store', response_string, re.IGNORECASE)
+      
+      # Should it be cached?
+      Find_cache = True
+      if no_store:
+        Find_cache = False
+        print("If it is no_store, then it cannot be cached.")
+      
+      # Should Cache
+      if Find_cache:
+        print("Caching")
+        # Create a new file  in the cache
+        cacheDir, file = os.path.split(cacheLocation)
+        print ('cached directory ' + cacheDir)
+        if not os.path.exists(cacheDir):
+          os.makedirs(cacheDir)
+        cacheFile = open(cacheLocation, 'wb')
 
-      # Create a new file in the cache for the requested file.
-      cacheDir, file = os.path.split(cacheLocation)
-      print ('cached directory ' + cacheDir)
-      if not os.path.exists(cacheDir):
-        os.makedirs(cacheDir)
-      cacheFile = open(cacheLocation, 'wb')
 
       # Save origin server response in the cache file
       # ~~~~ INSERT CODE ~~~~
-      cacheFile.write(originServerRespone)
+      cacheFile.write(originServerResponse) # Write HTTP response to a file
       # ~~~~ END CODE INSERT ~~~~
       cacheFile.close()
       print ('cache file closed')
